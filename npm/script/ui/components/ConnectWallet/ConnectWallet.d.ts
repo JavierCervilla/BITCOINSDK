cbe6d92c213b37b206b8f88ed5473454c3d8453e
javiercervilla <javier@cervilla.es>
2025-03-10 19:26:10 +0100

    npm=0.2.33

--- npm/script/ui/components/ConnectWallet/ConnectWallet.d.ts
+++
@@ -1,7 +0,0 @@
-declare class ConnectWalletButton extends globalThis.HTMLElement {
-    private readonly wallets;
-    constructor();
-    connectedCallback(): void;
-    private render;
-}
-export { ConnectWalletButton };
