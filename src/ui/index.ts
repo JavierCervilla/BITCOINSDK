9d5bae869e3152022105c492171f917031de916c
javiercervilla <javier@cervilla.es>
2025-03-10 18:41:11 +0100

    feat(ConnectWalletButton):[+] Add Web component export

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 import { WalletManager, useWallet } from './context/walletContext.ts';
 import { default as ConnectWalletButtonReact } from './components/ConnectWallet/ConnectWallet.react.tsx';
+import { ConnectWalletButton } from "./components/ConnectWallet/ConnectWallet.ts";
 import { walletConfig } from './providers/index.ts';
 
-export { WalletManager, useWallet, ConnectWalletButtonReact, walletConfig };
+export { WalletManager, useWallet, ConnectWalletButtonReact, ConnectWalletButton, walletConfig };
