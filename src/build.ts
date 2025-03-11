b0483df8ed41f92f009a1ed3fbf643c161a48fcc
javiercervilla <javier@cervilla.es>
2025-03-11 09:40:23 +0100

    npm=0.2.52

--- src/build.ts
+++ src/build.ts
@@ -28,7 +28,7 @@ async function main() {
         shims: { deno: false },
         package: {
             name: "bitcoinsdk",
-            version: "0.2.51",
+            version: "0.2.52",
             description: "Bitcoin SDK to integrate Bitcoin wallets to your app and get access to The OpenBook Protocol in your project",
             license: "MIT",
             repository: {
