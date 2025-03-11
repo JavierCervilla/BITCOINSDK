9a1397a4c7da3f3a9602be7adb5d1c042ba488c9
javiercervilla <javier@cervilla.es>
2025-03-11 07:52:59 +0100

    npm=0.2.41

--- npm/script/ui/components/ConnectWallet/ConnectWallet.d.ts
+++
@@ -1,9 +0,0 @@
-import type { WalletManagerInterface } from "../../context/walletContext.js";
-declare class ConnectWalletButton extends globalThis.HTMLElement {
-    private readonly wallets;
-    walletManager: WalletManagerInterface;
-    constructor();
-    connectedCallback(): void;
-    private render;
-}
-export { ConnectWalletButton };
--- npm/src/mod.ts
+++
@@ -1,2 +0,0 @@
-export * as core from "./core/index.js"
-export * as ui from "./ui/index.js"
