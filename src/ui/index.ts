c4ea8a3bc084bdbea7d527e5b87c8b7b3579b844
javiercervilla <javier@cervilla.es>
2025-03-11 09:40:07 +0100

    npm=0.2.52

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,4 +2,8 @@ import { WalletManager, useWallet } from './context/walletContext.ts';
 import { ConnectWalletButton } from "./components/ConnectWallet/ConnectWallet.ts";
 import { walletConfig, UNIVERSE_WALLET_LABEL } from './providers/index.ts';
 
+if (!customElements.get("connect-wallet-button")) {
+    customElements.define("connect-wallet-button", ConnectWalletButton);
+}
+
 export { WalletManager, useWallet, ConnectWalletButton, walletConfig, UNIVERSE_WALLET_LABEL };
