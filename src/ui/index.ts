39fbdd9d092033159fe70315787ec932399ed248
javiercervilla <javier@cervilla.es>
2025-03-11 07:46:19 +0100

    npm=0.2.40

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,4 @@
-import { WalletManager } from './context/walletContext.ts';
-import { useWallet } from './context/walletInstance.ts';
+import { WalletManager, useWallet } from './context/walletContext.ts';
 import { default as ConnectWalletButtonReact } from './components/ConnectWallet/ConnectWallet.react.tsx';
 import { ConnectWalletButton } from "./components/ConnectWallet/ConnectWallet.ts";
 import { walletConfig, UNIVERSE_WALLET_LABEL } from './providers/index.ts';
